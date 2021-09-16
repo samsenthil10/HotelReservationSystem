dd7b1a4114d44cd6ee6aa5a32a28bbc90ed8d736
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-16 08:12:22 +0530

    Added: To display welcome to hotel reservation page

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationMain {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("----------Welcome To Hotel Reservation System----------");
+	}
+}
