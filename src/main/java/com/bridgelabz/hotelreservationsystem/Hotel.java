2816c1a7803165f8dd667334826a77c336e1fe4d
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-18 05:08:09 +0530

    Added: To store hotel details

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class Hotel {
+	
+	private String hotelName;
+	private Integer ratesForRegularCustomer;
+	
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public Integer getRatesForRegularCustomer() {
+		return ratesForRegularCustomer;
+	}
+	public void setRatesForRegularCustomer(Integer ratesForRegularCustomer) {
+		this.ratesForRegularCustomer = ratesForRegularCustomer;
+	}
+}
