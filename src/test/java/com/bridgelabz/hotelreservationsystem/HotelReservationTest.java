2ded90696bbf542290a7ee46db38134143a08722
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-18 05:51:23 +0530

    Added: Test cases to validate exceptions due to rate field

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -12,8 +12,6 @@ public class HotelReservationTest {
 	@Test
 	public void givenName_WhenAddingHotel_ShouldIncreaseSize() {
 
-		try {
-			
 			LinkedHashSet<Hotel> listOfHotels = new LinkedHashSet<>();
 			Hotel hotel = new Hotel();
 			HotelReservationIF hotelReservationOperations = new HotelReservationImpl();
@@ -21,11 +19,6 @@ public class HotelReservationTest {
 			int size = (int) listOfHotels.stream().count();
 			listOfHotels.add(hotel);
 			Assert.assertEquals(listOfHotels.size(), size+1);
-		}
-		catch(HotelReservationExceptions e) {
-
-			Assert.assertEquals("Name cannot be Null!",exceptionType.NAME_ENTERED_NULL);			
-		}
 	}
 
 	@Test
@@ -53,12 +46,58 @@ public class HotelReservationTest {
 			LinkedHashSet<Hotel> listOfHotels = new LinkedHashSet<>();
 			Hotel hotel = new Hotel();
 			HotelReservationIF hotelReservationOperations = new HotelReservationImpl();
-			hotel = hotelReservationOperations.addNewHotel(null, 110);
+			hotel = hotelReservationOperations.addNewHotel("", 110);
 			listOfHotels.add(hotel);
 		}
 		catch(HotelReservationExceptions e) {
 
-			Assert.assertEquals("Name cannot be Null!",exceptionType.NAME_ENTERED_NULL);			
+			Assert.assertEquals("Name cannot be Empty!",exceptionType.NAME_ENTERED_EMPTY);			
+		}
+	}
+	
+	@Test
+	public void givenRateForRegularCustomer_WhenAddingHotel_ShouldIncreaseSize() {
+
+			LinkedHashSet<Hotel> listOfHotels = new LinkedHashSet<>();
+			Hotel hotel = new Hotel();
+			HotelReservationIF hotelReservationOperations = new HotelReservationImpl();
+			hotel = hotelReservationOperations.addNewHotel("Lakewood", 110);
+			int size = (int) listOfHotels.stream().count();
+			listOfHotels.add(hotel);
+			Assert.assertEquals(listOfHotels.size(), size+1);
+		}
+
+	@Test
+	public void givenRateForRegularCustomerNull_WhenAddingHotel_ShouldThrowRateForRegularCustomerNull() {
+
+		try {
+			
+			LinkedHashSet<Hotel> listOfHotels = new LinkedHashSet<>();
+			Hotel hotel = new Hotel();
+			HotelReservationIF hotelReservationOperations = new HotelReservationImpl();
+			hotel = hotelReservationOperations.addNewHotel("LakeWood", null);
+			listOfHotels.add(hotel);
+		}
+		catch(HotelReservationExceptions e) {
+
+			Assert.assertEquals("Rate for regular customer cannot be Null!",exceptionType.RATE_FOR_REGULAR_CUSTOMER_ENTERED_NULL);			
+		}
+	}
+	
+	@Test
+	public void givenRateForRegularCustomerNegative_WhenAddingHotel_ShouldThrowRateForRegularCustomerEmpty() {
+
+		try {
+			
+			LinkedHashSet<Hotel> listOfHotels = new LinkedHashSet<>();
+			Hotel hotel = new Hotel();
+			HotelReservationIF hotelReservationOperations = new HotelReservationImpl();
+			hotel = hotelReservationOperations.addNewHotel("Lakewood", -100);
+			listOfHotels.add(hotel);
+		}
+		catch(HotelReservationExceptions e) {
+
+			Assert.assertEquals("Rate for regular customer cannot be negative!",exceptionType.RATE_FOR_REGULAR_CUSTOMER_ENTERED_NEGATIVE);			
 		}
 	}
 }
