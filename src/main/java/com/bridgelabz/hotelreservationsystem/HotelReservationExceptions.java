5e7f520aa60e9777c22a29b7607d9c6b6235b285
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-19 19:19:58 +0530

    Refactored: Order of parameters in constructor

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationExceptions.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationExceptions.java
@@ -11,11 +11,12 @@ public class HotelReservationExceptions extends RuntimeException {
 		RATE_FOR_REGULAR_CUSTOMER_ENTERED_NEGATIVE,
 		DATE_CANNOT_BE_NULL,
 		DATE_CANNOT_BE_EMPTY,
+		DATE_IN_INVALID_FORMAT
 	}
 
 	exceptionType type;
 
-	public HotelReservationExceptions(String message, exceptionType type) {
+	public HotelReservationExceptions(exceptionType type, String message) {
 		super(message);
 		this.type = type;
 		System.out.println(message);
