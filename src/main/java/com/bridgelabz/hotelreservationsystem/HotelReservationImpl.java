2a3da5536266be88e390318f78c27e0101d5578c
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-19 19:20:57 +0530

    Refactored: To throw custom exception based on runtime exception

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationImpl.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationImpl.java
@@ -2,6 +2,7 @@ package com.bridgelabz.hotelreservationsystem;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
 import java.util.LinkedHashSet;
 
@@ -15,21 +16,22 @@ public class HotelReservationImpl implements HotelReservationIF {
 		Hotel hotel = new Hotel();
 		try {
 
-			if(name == null)
-				throw new HotelReservationExceptions("Hotel name cannot be Null!", exceptionType.NAME_ENTERED_NULL);
 			if(name.length()==0)
-				throw new HotelReservationExceptions("Hotel name cannot be Empty!", exceptionType.NAME_ENTERED_EMPTY);
-			if(ratesForRegularCustomer == null)
-				throw new HotelReservationExceptions("Rates for regular customer cannot be Null!", exceptionType.RATE_FOR_REGULAR_CUSTOMER_ENTERED_NULL);
-			if(ratesForRegularCustomer < 0)
-				throw new HotelReservationExceptions("Rates for regular customer cannot be Negative!", exceptionType.RATE_FOR_REGULAR_CUSTOMER_ENTERED_NEGATIVE);
-			else {
-				hotel.setHotelName(name);
-				hotel.setRatesForRegularCustomer(ratesForRegularCustomer);
-			}
-		}
-		catch(RuntimeException e) {
+				throw new HotelReservationExceptions(exceptionType.NAME_ENTERED_EMPTY, "Hotel name cannot be Empty!");
+			hotel.setHotelName(name);
 
+		}
+		catch(NullPointerException e) {
+			throw new HotelReservationExceptions(exceptionType.NAME_ENTERED_NULL,"Hotel name cannot be Null!");
+		}
+		try {
+
+			if(ratesForRegularCustomer < 0)
+				throw new HotelReservationExceptions(exceptionType.RATE_FOR_REGULAR_CUSTOMER_ENTERED_NEGATIVE,"Rates for regular customer cannot be Negative!");
+			hotel.setRatesForRegularCustomer(ratesForRegularCustomer);
+		}
+		catch(NullPointerException e) {
+			throw new HotelReservationExceptions(exceptionType.RATE_FOR_REGULAR_CUSTOMER_ENTERED_NULL,"Rates for regular customer cannot be Null!");
 		}
 		return hotel;
 	}
@@ -38,25 +40,31 @@ public class HotelReservationImpl implements HotelReservationIF {
 	@Override
 	public String findCheapestHotel(LinkedHashSet<Hotel> listOfHotels, String date1, String date2) {
 
-		if(date1==null || date2==null) {
-			throw new HotelReservationExceptions("Date cannot be Null!",exceptionType.DATE_CANNOT_BE_NULL);
-		}
-		if(date1.length()==0 || date2.length()==0) {
-			throw new HotelReservationExceptions("Date cannot be Empty!",exceptionType.DATE_CANNOT_BE_EMPTY);
-		}
-		else {
-			Hotel cheapestHotel = null;
+		Hotel cheapestHotel = null;
+		try {
+			if(date1.length()==0 || date2.length()==0) {
+				throw new HotelReservationExceptions(exceptionType.DATE_CANNOT_BE_EMPTY,"Date cannot be Empty!");
+			}
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
-			LocalDate startDate = LocalDate.parse(date1,formatter);
-			LocalDate endDate = LocalDate.parse(date2,formatter);
-			int totalDays =  (int) ChronoUnit.DAYS.between(startDate, endDate);
-			cheapestHotel = listOfHotels.stream()
-					.min((rate1,rate2) -> rate1.getRatesForRegularCustomer()*totalDays - rate2.getRatesForRegularCustomer()*totalDays)
-					.get();
+			try {
+				LocalDate startDate = LocalDate.parse(date1,formatter);
+				LocalDate endDate = LocalDate.parse(date2,formatter);
+				int totalDays =  (int) ChronoUnit.DAYS.between(startDate, endDate);
+				cheapestHotel = listOfHotels.stream()
+						.min((rate1,rate2) -> rate1.getRatesForRegularCustomer()*totalDays - rate2.getRatesForRegularCustomer()*totalDays)
+						.get();
 
-			if(cheapestHotel == null)
-				return "No Hotels Found";
-			return cheapestHotel.getHotelName();
-		} 
-	}
+			}
+			catch(DateTimeParseException e) {
+				throw new HotelReservationExceptions(exceptionType.DATE_IN_INVALID_FORMAT,"Invalid Date format!");
+			}
+
+		}
+		catch(NullPointerException e) {
+			throw new HotelReservationExceptions(exceptionType.DATE_CANNOT_BE_NULL,"Date cannot be NULL!");
+		}
+		if(cheapestHotel == null)
+			return "No Hotels Found";
+		return cheapestHotel.getHotelName();
+	} 
 }
