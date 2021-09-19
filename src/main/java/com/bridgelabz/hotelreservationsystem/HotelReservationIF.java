32b0ea863a7ea4b088c378b46936ca85129fd533
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-19 17:16:02 +0530

    Added: To find cheapest hotel between given date range

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import java.util.LinkedHashSet;
+
 public interface HotelReservationIF {
 	
 	public Hotel addNewHotel(String name, Integer ratesForRegularCustomer);
+	public String findCheapestHotel(LinkedHashSet<Hotel> listOfHotels, String startDate, String endDate);
 
 }
--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationImpl.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationImpl.java
@@ -1,15 +1,20 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.LinkedHashSet;
+
 import com.bridgelabz.hotelreservationsystem.HotelReservationExceptions.exceptionType;
 
 public class HotelReservationImpl implements HotelReservationIF {
 
 	@Override
 	public Hotel addNewHotel(String name, Integer ratesForRegularCustomer) {
-		
+
 		Hotel hotel = new Hotel();
 		try {
-			
+
 			if(name == null)
 				throw new HotelReservationExceptions("Hotel name cannot be Null!", exceptionType.NAME_ENTERED_NULL);
 			if(name.length()==0)
@@ -24,8 +29,34 @@ public class HotelReservationImpl implements HotelReservationIF {
 			}
 		}
 		catch(RuntimeException e) {
-			
+
 		}
 		return hotel;
+	}
+
+
+	@Override
+	public String findCheapestHotel(LinkedHashSet<Hotel> listOfHotels, String date1, String date2) {
+
+		if(date1==null || date2==null) {
+			throw new HotelReservationExceptions("Date cannot be Null!",exceptionType.DATE_CANNOT_BE_NULL);
+		}
+		if(date1.length()==0 || date2.length()==0) {
+			throw new HotelReservationExceptions("Date cannot be Empty!",exceptionType.DATE_CANNOT_BE_EMPTY);
+		}
+		else {
+			Hotel cheapestHotel = null;
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+			LocalDate startDate = LocalDate.parse(date1,formatter);
+			LocalDate endDate = LocalDate.parse(date2,formatter);
+			int totalDays =  (int) ChronoUnit.DAYS.between(startDate, endDate);
+			cheapestHotel = listOfHotels.stream()
+					.min((rate1,rate2) -> rate1.getRatesForRegularCustomer()*totalDays - rate2.getRatesForRegularCustomer()*totalDays)
+					.get();
+
+			if(cheapestHotel == null)
+				return "No Hotels Found";
+			return cheapestHotel.getHotelName();
+		} 
 	}
 }
